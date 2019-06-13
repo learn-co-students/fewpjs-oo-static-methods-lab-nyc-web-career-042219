0b12b78c793924d21bf6b2b1ea32532e2d395979
Jennifer Oh <ohjennifer.90@gmail.com>
2019-06-13 19:31:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g,'')
+  }
+  static titleize(string){
+    let wordArray = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the']
+    let array = string.split(' ')
+    return array.map(word => {
+      if (!wordArray.includes(word) || array.indexOf(word) === 0){
+        return this.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(' ')
+  }
 }
+
