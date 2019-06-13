be88f0c6a90843797b3ea4f754c44bba937ac905
henryk1229 <henryk1229@gmail.com>
2019-06-13 12:09:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9 '-]/g, '')
+  }
+  static titleize(string){
+    let articles = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let capString = Formatter.capitalize(string)
+    let newArray = capString.split(' ');
+    let capArray = [];
+    newArray.forEach(word => {
+        if (articles.indexOf(word) === -1) { capArray.push(Formatter.capitalize(word))}
+        else {capArray.push(word)}
+    })
+    return capArray.join(' ')
+  }
 }
